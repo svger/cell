60120b2c9002d1eb6c6d89e8ea687f92901d6773
Kane <xiaoyunhua@shhxzq.com>
2017-11-22 11:22:30 +0800

    ignore & demo build

--- demo/component.js
+++ demo/component.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import Demo from '../src/index';
+import Cell from '../src';
 
 class App extends React.Component {
 
   render() {
     return (
-       <div>
-         <div>Button Demo</div>
-
-       </div>
-
+      <div>
+        <div>Cell Demo</div>
+        <Cell>
+          <Cell.Title>添加银行卡</Cell.Title>
+          <Cell.Item>
+            <Cell.Left text="选择银行" />
+            <Cell.Right arrow>
+              <span>请选择</span>
+            </Cell.Right>
+          </Cell.Item>
+          <Cell.Item>
+            <Cell.Left text="银行卡号" />
+            <Cell.Right arrow>
+              <input type="tel" />
+            </Cell.Right>
+          </Cell.Item>
+        </Cell>
+      </div>
     );
   }
 }
